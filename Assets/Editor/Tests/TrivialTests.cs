600e4e141b089ddc743aa6c8cb527264402711d1
Ethan Kennerly <1338566+ethankennerly@users.noreply.github.com>
2025-07-21 16:51:21 -0400

    Testables defined an assembly.

---
+++ Assets/Editor/Tests/TrivialTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace EthanKennerly.PoorLife
+{
+    public class TrivialTests
+    {
+        [Test]
+        public void TestIsTrue()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
