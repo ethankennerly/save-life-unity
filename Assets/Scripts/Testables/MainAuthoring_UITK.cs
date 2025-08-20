56a39ea5987bda4c9c043cfaf7b5eecc0cdd2bb3
Ethan Kennerly <1338566+ethankennerly@users.noreply.github.com>
2025-08-20 09:44:54 -0700

    WIP: Main Authoring UITK referred to Age Up.

--- Assets/Scripts/Testables/MainAuthoring_UITK.cs
+++ Assets/Scripts/Testables/MainAuthoring_UITK.cs
@@ -3,14 +3,14 @@ using UnityEngine.UI;
 
 namespace EthanKennerly.SaveLife
 {
-    public class MainAuthoring : MonoBehaviour, IMainAuthoring
+    public class MainAuthoring_UITK : MonoBehaviour, IMainAuthoring
     {
         [SerializeField]
         private Transform _parent;
         public Transform Parent => _parent;
 
         [SerializeField]
-        private AgeUpAuthoring_uGUI _ageUp;
+        private AgeUpAuthoring_UITK _ageUp;
         public IAgeUpAuthoring AgeUp => _ageUp;
 
         [SerializeField]
---
+++ Assets/Scripts/Testables/MainAuthoring_uGUI.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace EthanKennerly.SaveLife
+{
+    public class MainAuthoring_uGUI : MonoBehaviour, IMainAuthoring
+    {
+        [SerializeField]
+        private Transform _parent;
+        public Transform Parent => _parent;
+
+        [SerializeField]
+        private AgeUpAuthoring_uGUI _ageUp;
+        public IAgeUpAuthoring AgeUp => _ageUp;
+
+        [SerializeField]
+        private GameObject _deathPopupPrefab;
+        public GameObject DeathPopupPrefab => _deathPopupPrefab;
+
+        [SerializeField]
+        TouchRecordingAuthoring _touchRecording;
+        public ITouchRecordingAuthoring TouchRecording => _touchRecording;
+
+        [SerializeField]
+        TouchReplayAuthoring _touchReplay;
+        public ITouchReplayAuthoring TouchReplay => _touchReplay;
+
+        private MainController _saveLife;
+
+        private void Awake()
+        {
+            _saveLife = new MainController(this);
+        }
+
+        private void LateUpdate()
+        {
+            if (_saveLife != null)
+            {
+                _saveLife.Update(Time.deltaTime);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (_saveLife != null)
+            {
+                _saveLife.Dispose();
+                _saveLife = null;
+            }
+        }
+    }
+}
